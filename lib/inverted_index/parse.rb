3f1b955939ce5b0d428bf39d2856a1d188511bbb
sfigart <sfigart@gmail.com>
2012-03-05 09:48:39 -1000

    Updated parse with date detection

--- lib/inverted_index/parse.rb
+++ lib/inverted_index/parse.rb
@@ -1,3 +1,5 @@
+require 'hpricot'
+
 module InvertedIndex
   class Parse
     attr_accessor :html, :doc, :body, :text, :tokens
@@ -18,8 +20,7 @@ module InvertedIndex
 	text = node.to_plain_text.strip
 	words = clean(text).split(' ')
 	words.each do |word|
-	  clean_word  = clean(word)
-	  @tokens << clean_word unless clean_word.empty?
+	  @tokens << word unless word.empty?
 	end
       end
       
@@ -27,12 +28,9 @@ module InvertedIndex
       @text = @tokens.join(' ')
     end
 
-    # TODO: Still need to clean more characters without losing
-    # context (e.g. dates, U.S.)
     def clean(text)
       # Replace new line and tabs with space
-      # Replace apostrophe with ''
-      return text.gsub(/(\n|\t)/,' ').gsub(/\'/,'').strip
+      return text.gsub(/(\n|\t)/,' ').strip
     end
   end
 end
